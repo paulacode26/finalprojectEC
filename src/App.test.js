91e4fa38b59af0406a10bbe4fa328e514e3dcc17
paulacode26 <pgperez26@gmail.com>
2025-03-31 14:11:17 -0600

    Create React App with group details and test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,18 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/group 1/i);
+  expect(titleElement).toBeInTheDocument();
+
+  const member1 = screen.getByText(/paula gomez perez/i);
+  expect(member1).toBeInTheDocument();
+
+  const member2 = screen.getByText(/sandra vera gomez/i);
+  expect(member2).toBeInTheDocument();
+
+  const member3 = screen.getByText(/nafiyad adane gudina/i);
+  expect(member3).toBeInTheDocument();
+
+  const member4 = screen.getByText(/erick mulia goycoolea/i);
+  expect(member4).toBeInTheDocument();
 });
